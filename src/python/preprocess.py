b4c8e206dc5828e2cfe9285992e3e4e584e0d910
just-pie <stefi7773@gmail.com>
2020-05-07 15:06:02 +0200

    preprocess.py pre XGBoost Pridanie metody na ziskanie informacii z datumov

--- src/python/preprocess.py
+++ src/python/preprocess.py
@@ -2,7 +2,7 @@ from sklearn.preprocessing import OrdinalEncoder
 
 class preprocess():
 
-    def categoricalToInteger(df):
+    def provinceNan(df):
         # vymazem null hodnoty z provincii
         df.Province_State.fillna('NaN', inplace=True)
         oe = OrdinalEncoder()
@@ -10,4 +10,13 @@ class preprocess():
             df.loc[:, ['Province_State', 'Country_Region', 'Continent']])
         return df
 
+    # z datumov ziskam dalsie informacie pre lepsie vizualne spracovanie
+    def date_formatting(df):
+        df['day'] = df['Date'].dt.day
+        df['month'] = df['Date'].dt.month
+        df['dayofweek'] = df['Date'].dt.dayofweek
+        df['dayofyear'] = df['Date'].dt.dayofyear
+        df['quarter'] = df['Date'].dt.quarter
+        df['weekofyear'] = df['Date'].dt.weekofyear
+        return df
 
